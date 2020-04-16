6c0f71e50e942b3d57cd23a93efa3b9ca0995da0
Ian Korf <ifkorf@ucdavis.edu>
2020-04-15 19:37:32 -0700

    edit

--- gc_win.py
+++ gc_win.py
@@ -3,6 +3,7 @@
 # Write a program that computes the GC fraction of a DNA sequence in a window
 # Window size is 11 nt
 # Output with 4 significant figures using whichever method you prefer
+# Use nested loops
 
 seq = 'ACGACGCAGGAGGAGAGTTTCAGAGATCACGAATACATCCATATTACCCAGAGAGAG'
 w = 11
