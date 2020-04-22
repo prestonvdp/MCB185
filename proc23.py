97b21e040f954cacfde9352600647480c29109dc
Ian Korf <ifkorf@ucdavis.edu>
2020-04-22 12:11:41 -0700

    new

---
+++ proc23.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import fileinput
+
+# Write a program that reads a 23andme file
+# Report the number of SNPs successfully assayed
+# Report the heterozygosity
+# Report the failure rate
+# https://opensnp.org/genotypes (make sure the data is 23andme)
+
+
+
+"""
+proc23.py ~/Downloads/9572.23andme.7848
+"""
